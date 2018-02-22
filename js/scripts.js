c578b525a9c34e4eb833fd90e7d49e555670fb34
Guest User <Guest@Epicodus-5B.local>
2018-02-21 19:56:36 -0800

    This will begin to define the output for the questions defined in the first section

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,11 @@ var numberOfSelected_A_Counter = 0;
 var numberOfSelected_B_Counter = 0;
 var numberOfSelected_C_Counter = 0;
 var userNameInput = "";
+
+userNameInput = $("#userName").val();
+$(".userNameOutput").text(userNameInput);
+userAgeInput = ($("#userAge").val());
+userFavoritePetInput = ($("#userFavoriteSeason").val());
+userFavoriteCountryInput = ($("#userFavoriteHoliday").val());
+userFavoriteIceCreamFlavorInput = ($("#userFavoriteFunActivity").val());
+userFavoriteSportInput = ($("#userPreferredPlaceToLive").val());
