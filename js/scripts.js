e013a77e724ca6fe0fcdf0face1c4c3be89bd8b2
Guest User <Guest@Epicodus-5B.local>
2018-02-21 19:29:25 -0800

    This will start the js document and define questions within jquery

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $("#mainForm").submit(function(event){
+    event.preventDefault();
+
+var userAgeInput ;
+var userFavoriteSeasonInput ;
+var userFavoriteHolidayInput ;
+var userFavoriteFunActivity ;
+var userFavoriteAnimal ;
+var userPreferredplacetolive
+var numberOfSelected_A_Counter = 0;
+var numberOfSelected_B_Counter = 0;
+var numberOfSelected_C_Counter = 0;
+var userNameInput = "";
