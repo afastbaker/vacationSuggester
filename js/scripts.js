1bdb4734269d8a0a35d70ba61e42620ef6d497bd
Alex Baker <afastbaker@gmail.com>
2018-02-25 11:51:09 -0800

    This will and If and else IF statements to js

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,47 @@ var userNameInput = "";
 userNameInput = $("#userName").val();
 $(".userNameOutput").text(userNameInput);
 userAgeInput = ($("#userAge").val());
-userFavoritePetInput = ($("#userFavoriteSeason").val());
-userFavoriteCountryInput = ($("#userFavoriteHoliday").val());
-userFavoriteIceCreamFlavorInput = ($("#userFavoriteFunActivity").val());
-userFavoriteSportInput = ($("#userPreferredPlaceToLive").val());
+userFavoriteSeasonInput = ($("#userFavoriteSeason").val());
+userFavoriteHolidayInput = ($("#userFavoriteHoliday").val());
+userFavoriteFunActivityInput = ($("#userFavoriteFunActivity").val());
+userFavoritePreferredPlaceToLiveInput = ($("#userPreferredPlaceToLive").val());
+
+if (userAgeInput === "a"){
+      numberOfSelected_A_Counter += 1;
+    } else if (userAgeInput === "b") {
+      numberOfSelected_B_Counter += 1;
+    } else if (userAgeInput === "c") {
+      numberOfSelected_C_Counter += 1;
+    }
+
+    if (userFavoriteSeasonInput === "a"){
+      numberOfSelected_A_Counter += 1;
+    } else if (userFavoriteSeasonInput === "b") {
+      numberOfSelected_B_Counter += 1;
+    } else if (userFavoriteSeasonInput === "c") {
+      numberOfSelected_C_Counter += 1;
+    }
+
+    if (userFavoriteHolidayInput === "a"){
+      numberOfSelected_A_Counter += 1;
+    } else if (userFavoriteHolidayInput === "b") {
+      numberOfSelected_B_Counter += 1;
+    } else if (userFavoriteHolidayInput === "c") {
+      numberOfSelected_C_Counter += 1;
+    }
+
+    if (userFavoriteFunActivityInput === "a"){
+      numberOfSelected_A_Counter += 1;
+    } else if (userFavoriteIceCreamFlavorInput === "b") {
+      numberOfSelected_B_Counter += 1;
+    } else if (userFavoriteIceCreamFlavorInput === "c") {
+      numberOfSelected_C_Counter += 1;
+    }
+
+    if (userFavoritePreferredPlaceToLiveInput === "a"){
+      numberOfSelected_A_Counter += 1;
+    } else if (userFavoritePreferredPlaceToLiveInput === "b") {
+      numberOfSelected_B_Counter += 1;
+    } else if (userFavoritePreferredPlaceToLiveInput === "c") {
+      numberOfSelected_C_Counter += 1;
+    }
